3e7c2d1bdee50c4183b466ab523e8c328f0cf670
Jackson Ludwig <jacksonludwig0@gmail.com>
2023-08-16 22:34:16 -0400

    some todos for charge point

--- lib/v16/charge_point.ex
+++ lib/v16/charge_point.ex
@@ -1,10 +1,52 @@
 defmodule MessageHandling.ChargePoint do
-  # TODO
+  @moduledoc """
+  This process handles responding to requests from the central system.
+  """
+  alias MessageStream.EventBus
+  use GenServer
 
-  # def handle_cs_call("TriggerMessage", message = %RequestResponse{}) do
-  # end
+  # TODO: actually handle messages
+  # TODO: store state here or in other process?
 
-  def handle_cs_call(unknown_message, _message) do
-    {:error, :unknown_call_from_cs, unknown_message}
+  # Client
+
+  def start_link(_args) do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
+  # Server
+
+  @impl true
+  def init(state) do
+    EventBus.listen_for(:from_cs)
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info({:broadcasted_message, :from_cs, data}, state) do
+    if is_struct(data, %RequestResponse{}) do
+      handle_cs_call(data.action, data)
+    end
+
+    {:noreply, state}
+  end
+
+  # Message Handling
+
+  def handle_cs_call(msg = %RequestResponse{}) when msg.action == "RemoteStartTransaction" do
+    # 1. send remote start conf
+    # 2. start transaction flow
+  end
+
+  def handle_cs_call(msg = %RequestResponse{}) when msg.action == "TriggerMessage" do
+    # 1. send connector status
+  end
+
+  def handle_cs_call(msg = %RequestResponse{}) when msg.action == "GetConfiguration" do
+    # 1. send configuration
+  end
+
+  def handle_cs_call(msg) do
+    {:error, :unknown_call_from_cs, msg.action}
   end
 end
